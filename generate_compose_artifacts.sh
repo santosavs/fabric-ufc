e384f0a86290a2d12ef14a73aca0cfb9f1d47fcf
Adriano V. dos Santos <adriano.santos@serpro.gov.br>
2019-05-14 14:22:04 -0300

    Correção na sequência de comandos.

--- generate_compose_artifacts.sh
+++ generate_compose_artifacts.sh
@@ -4,6 +4,7 @@
 # signcerts - certificado Admin da organização
 # keystore - chave privada
 PPGETI_ADMINCERT_PATH=crypto-config/peerOrganizations/ppgeti.ufc.br/users/Admin@ppgeti.ufc.br/msp/signcerts/Admin@ppgeti.ufc.br-cert.pem
+PPGETI_BNA_PATH=
 PPGETI_PK_PATH=crypto-config/peerOrganizations/ppgeti.ufc.br/users/Admin@ppgeti.ufc.br/msp/keystore
 PPGETI_PK=$(ls crypto-config/peerOrganizations/ppgeti.ufc.br/users/Admin@ppgeti.ufc.br/msp/keystore | grep _sk)
 
@@ -13,29 +14,29 @@ composer card create -p connection-ppgeti.json \
 -r PeerAdmin -r ChannelAdmin
 
 # Importação do Admin Card
-composer card import -f PeerAdmin@ufc-network.card
-
-# Instalando o runtime no peer
-composer runtime install -c PeerAdmin@ufc-network.card -n ufc-network
+composer card import -f PeerAdmin@ufc.card
 
 # Requisitando credenciais para o usuário da business network
-composer identity request -c PeerAdmin@ufc-network -u admin -s adminpw -d ppgetiuser
+composer identity request -c PeerAdmin@ufc -u admin -s adminpw -d ppgetiuser
+
+# Instalação do BNA
+composer network install -c PeerAdmin@ufc.card -a ${PPGETI_BNA_PATH}
 
 # Iniciando a business network
-composer network start -c PeerAdmin@ufc-network \
--a ufc-network@0.0.1.bna \
+composer network start -c PeerAdmin@ufc \
+-a ufc@0.0.1.bna \
 -o endorsementPolicyFile=endorsement-policy.json \
 -A ppgetiuser -C ppgetiuser/admin-pub.pem
 
 # Gerando um card para acessar a rede como PPGETI
 composer card create -p connection-ppgeti.json \
 -u ppgetiuser \
--n ufc-network \
+-n ufc \
 -c ppgetiuser/admin-pub.pem \
 -k ppgetiuser/admin-priv.pem
 
 # Importando o card criado anteriormente
-composer card import -f ppgetiuser@ufc-network.card
+composer card import -f ppgetiuser@ufc.card
 
 # Testando a conectividade com a rede
-composer network ping -c ppgetiuser@ufc-network
+composer network ping -c ppgetiuser@ufc
